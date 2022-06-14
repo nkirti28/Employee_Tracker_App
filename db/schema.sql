a959bb21260433091c0c7a4d3b7ad4e457a97cee
nkirti28 <nalawade.kirti@gmail.com>
2022-06-14 09:59:41 -0500

    add schema.sql

---
+++ db/schema.sql
@@ -0,0 +1,30 @@
+DROP if DATABASE EXISTS employee_db;
+
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
+-- department table --
+CREATE TABLE department(
+  id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  dept_name VARCHAR(30) 
+);
+
+-- role table --
+CREATE TABLE role(
+  id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30),
+  salary DECIMAL,
+  department_id INT,
+  FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL 
+);
+
+CREATE TABLE employee(
+  id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  manager_id INT,
+  role_id INT,
+  FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
+  FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL,
+);
